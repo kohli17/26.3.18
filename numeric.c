3206b39e0b2c043b90df4c3c6f9a2e75a236d8aa
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-03-26 15:53:04 +0530

    Create numeric.c
    print the number

---
+++ numeric.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[10];
+int flag=0,i;
+scanf("%s",a);
+for(i=0;a[i]!='\0';i++)
+{
+    if(a[i]>='0'||a[i]<='9')
+    {
+        flag=1;
+    }
+}
+if(flag==1)
+printf("\nYES");
+else
+printf("\n No");
+}
